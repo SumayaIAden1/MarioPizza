576754f7253dc33d5000102630e66221a4f76c37
SumayaIAden1 <sumaya73tvs@gmail.com>
2024-10-29 15:58:26 +0100

    Finished total income

--- src/Bestillingsliste.java
+++ src/Bestillingsliste.java
@@ -7,12 +7,12 @@ import java.time.format.DateTimeFormatter;
 //PRECONDITION: ITEMS ARE ONLY REMOVED WHEN PAID FOR.
 
 public class Bestillingsliste {
-    private ArrayList<Ordre> ordreList = new ArrayList<>();
+    private ArrayList<Ordre> bestillingsListeArray = new ArrayList<>();
     private ArrayList<Ordre> ordrerHistorikArray = new ArrayList<>();
     private int currentOrderNr = 1;
 
     public void addOrdreToList(Ordre ordre) {
-        ordreList.add(ordre);
+        bestillingsListeArray.add(ordre);
         currentOrderNr++;
         writeBestillingsliste(); // Write to file when adding
     }
@@ -22,11 +22,11 @@ public class Bestillingsliste {
     }
 
     public void removeOrdreByNumber(int ordreNumber) {
-        for (int i = 0; i < ordreList.size(); i++) {
-            Ordre o = ordreList.get(i);
+        for (int i = 0; i < bestillingsListeArray.size(); i++) {
+            Ordre o = bestillingsListeArray.get(i);
             if (o.getOrdreNr() == ordreNumber) {
                 ordrerHistorikArray.add(o); // Add to removed orders list
-                ordreList.remove(i); // Remove from current orders
+                bestillingsListeArray.remove(i); // Remove from current orders
                 writeBestillingsliste(); // Update the orders file
                 writeRemovedOrdersToFile(); // Write all removed orders to the file
                 System.out.println("Order number " + ordreNumber + " has been removed.");
@@ -35,26 +35,41 @@ public class Bestillingsliste {
         }
     }
 
-    public void writeBestillingsliste() {
+    public void writeBestillingsliste()
+    {
         File bestillingsListe = new File("/Users/bruger/Desktop/UNI/1. semester/Programmering/IntelliJ/MarioPizza/src/Bestillingsliste.txt");
+
         DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH.mm");
         DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd-MM-yy");
 
         try (FileWriter writer = new FileWriter(bestillingsListe, false)) {
-            for (Ordre o : ordreList) {
-                Pizza pizza = o.getPizzaObject();
-                writer.append(String.format("ORDRE:\n" +
-                                " - ordre Number: %d\n" +
-                                " - bestillings Tid: %s\n" +
-                                " - bestillings Dato: %s\n" +
+            if (bestillingsListe.length() == 0) {
+                writer.write("ACTIVE ORDERS / BESTILLINGSLISTE \n\n");
+            }
+            for (int i = 0; i < this.bestillingsListeArray.size(); i++) {
+                Ordre o = this.bestillingsListeArray.get(i);
+                Pizza pizza = o.getPizzaObject(); // Get
+
+                //UDSKRIVER TEKSTEN PÅ EN BESTEMT MÅDE. LINJERNE PASSER SAMMEN.
+
+
+                writer.append(String.format(
+                        "ORDRE:\n" +
+                                " - Ordre Number: %d\n" +
+                                " - Bestillings Tid: %s\n" +
+                                " - Bestillings Dato: %s\n" +
+
                                 " - Pizza: %s, Price: %d, Nr: %d\n",
+
                         o.getOrdreNr(),
                         o.getBestillingsTid().format(timeFormatter),
                         o.bestillingsDato().format(dateFormatter),
-                        pizza.getPizzaName(),
-                        pizza.getPizzaPrice(),
-                        pizza.getPizzaNumber()));
+
+                        pizza.getPizzaName(), pizza.getPizzaPrice(), pizza.getPizzaNumber()));
+
                 writer.append(System.lineSeparator());
+
+                //System.out.println(o.getPizzaObject() + ", " + o.getBestillingsTid() + ", " + o.getOrdreNr());
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -65,8 +80,12 @@ public class Bestillingsliste {
         File removedOrdersFile = new File("/Users/bruger/Desktop/UNI/1. semester/Programmering/IntelliJ/MarioPizza/src/OrdrerHistorik.txt");
         DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH.mm");
         DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd-MM-yy");
+        int totalPrice = 0;
 
         try (FileWriter writer = new FileWriter(removedOrdersFile, false)) {
+            if(removedOrdersFile.length() == 0){
+                writer.write("FINISHED / PAID FOR \n \n");
+            }
             for (int i = 0; i < ordrerHistorikArray.size(); i++) {
                 Ordre o = ordrerHistorikArray.get(i);
                 Pizza pizza = o.getPizzaObject();
@@ -82,14 +101,20 @@ public class Bestillingsliste {
                         pizza.getPizzaPrice(),
                         pizza.getPizzaNumber()));
                 writer.append(System.lineSeparator());
+
+                totalPrice += pizza.getPizzaPrice();
             }
+
+            writer.append("TOTAL INCOME FOR TODAY; \n");
+            writer.append(String.format("%d DKK\n", totalPrice));
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public void printRemovedOrders() {
-        System.out.println("Removed Orders:");
+        System.out.println("Paid and closed Orders:");
         for (int i = 0; i < ordrerHistorikArray.size(); i++) {
             Ordre ordre = ordrerHistorikArray.get(i);
             System.out.println(ordre);
